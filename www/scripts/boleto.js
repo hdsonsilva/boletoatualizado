1736c2b00e2d785a3728caef0ad0106eb17c62e1
Hudson <hudson@doctum.edu.br>
2016-04-24 18:26:35 -0300

    OK adicionando boleto.js

---
+++ www/scripts/boleto.js
@@ -0,0 +1,11 @@
+function digitorVerificadorLinhaDigitavel(){
+
+    var fatores = new Array(4, 3, 2, 9, 0, 8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2);
+    var soma = 0 ;
+    var i ;
+
+    for (i = 0; i < fatores.length; i++) { 
+
+    }
+    alert(i);
+}
